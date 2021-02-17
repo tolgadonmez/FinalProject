02164f1139aaeb6a2af18531b169711fe74c88ee
tolgadonmez <donmeztolga1907@gmail.com>
2021-02-18 00:07:23 +0300

    18/02/2021

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -11,12 +11,40 @@ namespace ConsoleUI
     {
         static void Main(string[] args)
         {
+            ProductTest();
+            //CategoryTest();
+
+        }
+
+        private static void CategoryTest()
+        {
+            CategoryManager categoryManager = new CategoryManager(new EfCategoryDal());
+            foreach (var category in categoryManager.GetAll())
+            {
+                Console.WriteLine(category.CategoryName);
+            }
+        }
+
+        private static void ProductTest()
+        {
             ProductManager productManager = new ProductManager(new EfProductDal());
 
-            foreach (var product in productManager.GetByUnitPrice(40,100))
+            var result = productManager.GetProductDetails();
+
+            if (result.Success == true)
             {
-                Console.WriteLine(product.ProductName);
+                foreach (var product in result.Data)
+                {
+                    Console.WriteLine(product.ProductName + "/" + product.CategoryName);
+                }
             }
+            else
+            {
+                Console.WriteLine(result.Message);
+            }
+            
+
+            
         }
     }
 }
--- DataAccess/Abstract/IProductDal.cs
+++ DataAccess/Abstract/IProductDal.cs
@@ -1,4 +1,6 @@
-﻿using Entities.Concrete;
+﻿using Core.DataAccess;
+using Entities.Concrete;
+using Entities.DTOs;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -8,6 +10,8 @@ namespace DataAccess.Abstract
     //veritabanında product ile alakalı yapılacak operasyonları içeren interface
     public interface IProductDal: IEntityRepository<Product>
     {
-       
+        List<ProductDetailDto> GetProductDetails();
     }
 }
+
+//Code Refactoring
